4f1e8b40630b139f9f54357999e63260528b1c1d
Vlodzjo <volodyalarych24@gmail.com>
2021-09-14 13:40:59 +0300

    DEV-171 : sadasdsadas

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.print("JDF");
+        System.out.print("hello world222323");
+        System.out.println();
     }
 }
