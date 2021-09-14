abbb52dd0450dae01ac7c0030d500c608f75f7c8
Vlodzjo <volodyalarych24@gmail.com>
2021-09-14 13:48:28 +0300

    DEV-171 : in new branch

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.print("hello world222323");
+        System.out.print("hello world2223236754545");
         System.out.println();
     }
 }
